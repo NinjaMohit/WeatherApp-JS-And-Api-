c1d19f9953b23edc45bd281eb22a4ef6b7711099
mohitspawar13 <mohitspawar13@gmail.com>
2024-02-18 23:23:06 +0530

    Done

--- WeatherApp/script.js
+++ WeatherApp/script.js
@@ -23,10 +23,7 @@ const options = {
 const getWeather=(city)=>{
     cityName.innerHTML=city
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city,options)
-//fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +Shanghai,options)
-//fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +Mumbai,options)
-//fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +Pune,options)
-//fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +Kolkata,options)
+
 
 .then(response=>response.json())
 .then(response=>{
@@ -46,15 +43,7 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city,opti
     wind_speed.innerHTML=response.wind_speed
     wind_speed2.innerHTML=response.wind_speed
 
-    feels_like3.innerHTML=response.feels_like
-    humidity3.innerHTML=response.humidity
-    max_temp3.innerHTML=response.max_temp
-    min_temp3.innerHTML=response.min_temp
-    sunrise3.innerHTML=response.sunrise
-    sunset3.innerHTML=response.sunset
-    temp3.innerHTML=response.temp
-    wind_degrees3.innerHTML=response.wind_degrees
-    wind_speed3.innerHTML=response.wind_speed
+    
 
 })
 .catch(err=>console.error(err));
